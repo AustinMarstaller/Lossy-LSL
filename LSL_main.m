f8eb07668b67359d1c6d831d3bf7a7efd2d1dee5
Austin Marstaller <amarstaller@smu.edu>
2024-05-04 14:53:37 -0500

    Investigating stiffness matrix. Cleaned up code a little.

--- LSL_main.m
+++ LSL_main.m
@@ -3,7 +3,7 @@ close all
 tic
 %% Synthetic data construction
 M=9; % M+1 total grid points
-h=1/M; % Grid point spacing. Implement h^hats
+h=1/M; % Grid point spacing. 
 x=(0:h:1)'; % Lattice in column vector
 
 mu = 0.35;
@@ -18,8 +18,8 @@ u_lambda_reference = zeros(M+1,numel(lambda));
 
 for j = 1:numel(lambda)
     % Each column is a solution for a particular value of lambda
-    [u_lambda(:,j),A] = LSL_FD(M,p,x,h,lambda(j)); % (u_j(0), u_j(1), ..., u_j(M))^T
-    [u_lambda_reference(:,j),C] = LSL_FD(M,p_reference,x,h,lambda(j));
+    [u_lambda(:,j),A] = LSL_FD(M,p,h,lambda(j)); % (u_j(0), u_j(1), ..., u_j(M))^T
+    [u_lambda_reference(:,j),C] = LSL_FD(M,p_reference,h,lambda(j));
 end
 
 D = ones(1,M+1)*h;
@@ -37,11 +37,17 @@ for i = 1:numel(lambda)
     dF_dlambda(i) = -u_lambda(:,i)' * D * u_lambda(:,i);
 end
 
-
 %% Mass & Stiffness matrices
 % M & S are symmetric w.r.t <-,->_D
-Mass      = -diag(dF_dlambda);
-Stiffness = (dF_dlambda)*diag(lambda) + dF_dlambda; % lambda dF/dlambda + dF/dlambda
+% M_ii = -u^T D u
+% S_ii = lambda dF/dlambda + F
+Mass      = -diag(dF_dlambda); 
+Stiffness = diag((dF_dlambda)*diag(lambda) + F); % lambda dF/dlambda + F
+
+% Mass(i,j) ?= u_i^T D u_j =: benchmark_Mass,
+% Stiffness(i,j) ?= u_i^T D A u_j =: benchmark_Stiffness
+benchmark_Mass = Mass*0;
+benchmark_Stiffness = Stiffness*0;
 
 for i = 1:numel(lambda)
     for j = 1:numel(lambda)
@@ -49,30 +55,32 @@ for i = 1:numel(lambda)
             Mass(i,j) = (F(i) - F(j))/(lambda(j) - lambda(i));
             Stiffness(i,j) = (F(j)*lambda(j) - F(i)*lambda(i))/(lambda(j) - lambda(i));
         end
+            benchmark_Mass(i,j) = u_lambda(:,i)' * D * u_lambda(:,j);
+            benchmark_Stiffness(i,j) = u_lambda(:,i)' * D * A * u_lambda(:,j); 
     end
 end
 
 
-%% Benchmark test: D = diag(h_0^, ..., h_(M)^): 
-% M(i,j) ?= u_i^T D u_j,
-% S(i,j) ?= u_i^T D A u_j
+%% Finite difference scheme to solve the BVP: -u'' + (p(x) + lambda)u = g(x), u'(0)=-1, u'(1)=0
 
-benchmark_Mass = Mass*0;
-benchmark_Stiffness = Stiffness*0;
-
-for i = 1:numel(lambda)
-    for j = 1:numel(lambda)
-        benchmark_Mass(i,j) = u_lambda(:,i)' * D * u_lambda(:,j);
-        benchmark_Stiffness(i,j) = u_lambda(:,i)' * D * A * u_lambda(:,j); 
-    end
+function [u, A] = LSL_FD(M,p,h,lambda)
+    % Coefficient matrix A is M+1 x M+1
+    A_diag = 2/h^2 * eye(M+1,M+1) + diag(p) + lambda * eye(M+1,M+1); 
+    A = spdiags([-1/h^2 0 -1/h^2],-1:1,M+1,M+1) + A_diag;
+    A(1,2) = -2/h^2;
+    A(M+1,M) = -2/h^2;
+    % Construct right-hand-side Mx1 vector f =(2/h, 0, ..., 0)^T 
+    f = zeros(M+1,1);
+    f(1)=2/h;
+    
+    % Solve the linear system Au = f
+    u = A\f;
 end
+
 
 n=10;
 b = rand(n,1); % vector of length n = 10
-[Q,alpha,beta] = Lanczos(A,b,n);
-%full(Q' * B * Q) % Verify T = Q' A Q
-% Also check Q^T Q = Identity
-%full(Q' * Q)
+[Q,alpha,beta] = Lanczos(A,b,n); % Perform Lanczos for change of basis: QV
 
 %% Lanczos algorithm
 function [Q, alpha, beta] = Lanczos(A,b,iter)
@@ -103,4 +111,5 @@ function [Q, alpha, beta] = Lanczos(A,b,iter)
         end
     end
 end
+
 toc
